eef22cd5b6d07769a050cd18052a7c8009dfa4d0
NathanCHEFF <54266398+NathanCHEFF@users.noreply.github.com>
2020-12-01 07:47:26 +0200

    create new file
    create new file

---
+++ script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+#   upload needs program
+sudo apt-get -y install lighttpd php-common php-cgi php vsftpd
+sudo lighty-enable-mod fastcgi-php
+
+#   set permissions
+chown -R www-data:www-data /var/www
+
+#   edit and add config file
+sed -i 's/\/var\/log\/lighttpd\/error.log//g' /etc/lighttpd/lighttpd.conf
+#sed -i 's/\/var\/log\/lighttpd\/error.log/path\/to\/error.log/g' /etc/lighttpd/lighttpd.conf
+
+PHP_CONFIG=`php -r 'echo php_ini_loaded_file();'`
+
+echo display_errors = On >> $PHP_CONFIG
+echo error_log = /var/www/html/log/php_errors.log >> $PHP_CONFIG
+echo >> $PHP_CONFIG
+echo extension=curl.so >> $PHP_CONFIG
+echo extension=mbstring.so >> $PHP_CONFIG
+
+mkdir /var/www/html/log/ -m 755
+echo >> /var/www/html/log/php_errors.log
+echo >> /var/www/html/log/crn_errors.log
+
+service lighttpd force-reload
+
+(crontab -l ; echo "@reboot sleep 60 /usr/bin/wget -t 3 -O - 'http://192.168.0.130/1.php' > /var/www/html/log/crn_errors.log")| crontab -
+reboot
